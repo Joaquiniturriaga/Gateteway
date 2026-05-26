68c68ded5d28b8401c3b6135576bb7a0cf299125
Joaquiniturriaga <joa.iturriaga@duocuc.cl>
2026-05-26 17:12:47 -0400

    (fix):ID:2-GATEWAY: Implementacion de clave compartida y evitar que se consulta directo al puerto del los micro servicios

--- src/main/java/com/java/gatewayy/filter/JwtFilter.java
+++ src/main/java/com/java/gatewayy/filter/JwtFilter.java
@@ -19,11 +19,19 @@ public class JwtFilter extends OncePerRequestFilter {
     @Value("${jwt.secret}")
     private String secret;
 
+    //Clave compartida que el gateway agrega a todos los request
+    //Los microservices verifican este header para asegurarse de que el requeste viene del gate
+    //Y no directamente del puerto 
+    @Value("{internal.secret}")
+    private String internalSecret;
+
     private static final List<String> RUTAS_PUBLICAS = List.of(
         "/api/auth/register",
         "/api/auth/login",
         "/"
     );
+
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest req,
@@ -75,6 +83,8 @@ public class JwtFilter extends OncePerRequestFilter {
             res.getWriter().write("{\"error\":\"Token invalido o expirado\"}");
             return;
         }
+
+        req.setAttribute("internalSecret", internalSecret);
 
         chain.doFilter(req, res);
     }
